bd2fc33b0773eb9604820644c95721ce3115a703
Paul COMTE <paul.codepro@gmail.com>
2023-03-18 13:21:22 +0100

    :sparkle: added width and height selector

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,85 @@
+use image::{DynamicImage, GenericImageView};
 use std::env;
 use std::path::Path;
 
-use image::{DynamicImage, GenericImageView, RgbaImage};
+struct Settings {
+    path: String,
+    img: DynamicImage,
+    width: u32,
+    height: u32,
+    num_rows: u32,
+    num_cols: u32,
+    row_height: u32,
+    col_width: u32,
+}
+
+impl Settings {
+    fn init(args: Vec<String>) -> Self {
+        let input_path = Path::new(&args[1]);
+        let output_dimensions: Vec<_> = args[2]
+            .split(':')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        let output_num: Vec<_> = args[3]
+            .split(':')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        let img = image::open(input_path).unwrap();
+        let (img_width, img_height) = img.dimensions();
+
+        Self {
+            path: input_path.file_stem().unwrap().to_str().unwrap().into(),
+            img,
+            width: output_dimensions[0],
+            height: output_dimensions[1],
+            num_rows: output_num[0],
+            num_cols: output_num[1],
+            row_height: img_height / output_num[0],
+            col_width: img_width / output_num[1],
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        eprintln!("Usage: {} <input-image> <rows:cols>", args[0]);
+    if args.len() != 4 {
+        eprintln!(
+            "Usage: {} <input-image> <width:height> <rows:cols>",
+            args[0]
+        );
         return;
     }
 
-    let input_path = Path::new(&args[1]);
-    let output_dimensions: Vec<_> = args[2]
-        .split(':')
-        .map(|x| x.parse::<u32>().unwrap())
-        .collect();
+    let settings = Settings::init(args);
 
-    let img = image::open(input_path).unwrap();
+    let resized = settings.img.resize_exact(
+        settings.col_width * settings.num_cols,
+        settings.row_height * settings.num_rows,
+        image::imageops::FilterType::Lanczos3,
+    );
 
-    let (img_width, img_height) = img.dimensions();
-    let num_rows = output_dimensions[0];
-    let num_cols = output_dimensions[1];
-    let row_height = img_height / num_rows;
-    let col_width = img_width / num_cols;
-    for i in 0..num_rows {
-        for j in 0..num_cols {
-            let row_start = i * row_height;
-            let col_start = j * col_width;
+    for i in 0..settings.num_rows {
+        for j in 0..settings.num_cols {
+            let row_start = i * settings.row_height;
+            let col_start = j * settings.col_width;
 
-            let imgbuf = DynamicImage::ImageRgb8(img.to_rgb8())
-                .crop(col_start, row_start, col_width, row_height)
+            let imgbuf = DynamicImage::ImageRgb8(resized.to_rgb8())
+                .crop(
+                    col_start,
+                    row_start,
+                    settings.col_width,
+                    settings.row_height,
+                )
+                .resize_exact(
+                    settings.width,
+                    settings.height,
+                    image::imageops::FilterType::Lanczos3,
+                )
                 .to_rgb8();
 
-            let output_path = format!(
-                "{}_{}_{}.png",
-                input_path.file_stem().unwrap().to_str().unwrap(),
-                i,
-                j
-            );
+            let output_path = format!("{}_{}_{}.png", settings.path, i, j);
             imgbuf.save(output_path).unwrap();
         }
     }
